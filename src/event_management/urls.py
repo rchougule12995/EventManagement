0d5c0c29c45b9a8116a79d925a869be7d5575dda
Rohit Chougule <rchougule12995@gmail.com>
2019-11-21 20:54:08 +0000

    Changed folder structure

--- src/event_management/urls.py
+++ src/event_management/urls.py
@@ -1,4 +1,4 @@
-"""try_django URL Configuration
+"""event_management URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- src/event_management/wsgi.py
+++ src/event_management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for try_django project.
+WSGI config for event_management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'try_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_management.settings')
 
 application = get_wsgi_application()
